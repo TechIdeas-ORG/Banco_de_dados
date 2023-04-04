379f55167ab2d2f5f7b9807bbbad91619b1fec16
Thigos Rodrigues <67590378+Thigos@users.noreply.github.com>
2023-04-03 23:15:42 -0300

    Update create-db-template.sql

--- create-db-template.sql
+++ create-db-template.sql
@@ -1,92 +1,32 @@
--- Active: 1676926302033@@127.0.0.1@3306@bd_smfp
 CREATE DATABASE bd_SMFP;
 
 USE bd_SMFP;
 
 CREATE TABLE tbUsuario(
     idUsuario INT PRIMARY KEY AUTO_INCREMENT
-    ,nome_usuario VARCHAR(50) NOT NULL
-    ,email_usuario VARCHAR(100) NOT NULL UNIQUE
-    ,senha_usuario VARCHAR(50) NOT NULL
-    ,telefone_usuario VARCHAR(15) NOT NULL
+    ,nomeUsuario VARCHAR(50) NOT NULL
+    ,emailUsuario VARCHAR(100) NOT NULL UNIQUE
+    ,senhaUsuario VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE tbConfig(
     idConfig INT PRIMARY KEY AUTO_INCREMENT
-    ,tempo_dispersao INT
+    ,tempoDispersao INT
 );
 
 CREATE TABLE tbAmbiente(
     idAmbiente INT PRIMARY KEY AUTO_INCREMENT
-    ,nome_ambiente VARCHAR(50)
-    ,desc_ambiente VARCHAR(150)
+    ,idUsuario INT
+    ,nomeAmbiente VARCHAR(50)
+    ,descAmbiente VARCHAR(150)
+    ,idConfig INT 
+    ,CONSTRAINT fk_tbAmbiente_idConfig FOREIGN KEY (idConfig) REFERENCES tbConfig(idConfig)
+    ,CONSTRAINT fk_tbAmbiente_idUsuario FOREIGN KEY (idUsuario) REFERENCES tbUsuario(idUsuario)
 );
 
 CREATE TABLE tbSensor(
     idSensor INT PRIMARY KEY AUTO_INCREMENT
-    ,porta_sensor VARCHAR(6) NOT NULL
+    ,idAmbiente INT
+    ,portaSensor VARCHAR(6) NOT NULL
+    ,CONSTRAINT fk_tbSensor_idAmbiente FOREIGN KEY (idAmbiente) REFERENCES tbAmbiente(idAmbiente)
 );
-
-CREATE TABLE tbMetricas (
-    idMetrica INT PRIMARY KEY AUTO_INCREMENT
-    ,dateMetrica DATETIME NOT NULL
-    ,valMetrica FLOAT
-);
-
-INSERT INTO tbUsuario (nome_usuario, email_usuario, senha_usuario, telefone_usuario)
-VALUES ('Admin', 'admin@techIdeas.com', 'admin', '(11) 9999-9999')
-       ,('Itaquera', 'itaquera@shopping.com', 'itaquera123', '(11) 1111-1111')
-       ,('Aricanduva', 'aricanduva@shopping.com', 'aricanduva123', '(11) 2222-2222');
-
-INSERT INTO tbAmbiente (nome_ambiente, desc_ambiente)
-VALUES ('Praça de Alimentação', 'Área de alimentação com diversas opções de restaurantes.')
-    ,('Cinema', 'Salas de cinema com as últimas novidades do cinema mundial.')
-    ,('Lojas de Departamento', 'Área com diversas lojas de departamento.')
-    ,('Estacionamento', 'Área de estacionamento para veículos.')
-    ,('Livraria', 'Loja especializada em livros de todos os gêneros.')
-    ,('Academia', 'Academia com equipamentos modernos e treinadores especializados.')
-    ,('Brinquedoteca', 'Espaço para crianças se divertirem com diversos brinquedos.')
-    ,('Cafeteria', 'Cafeteria com diversas opções de bebidas e comidas rápidas.')
-    ,('Salão de Beleza', 'Salão de beleza com profissionais qualificados.')
-    ,('Pet Shop', 'Loja especializada em produtos e serviços para animais de estimação.')
-    ,('Boutique', 'Loja com roupas e acessórios de grife.')
-    ,('Teatro', 'Espaço para apresentações teatrais e shows musicais.');
-    
-INSERT INTO tbConfig (tempo_dispersao)
-	VALUES (5), 
-		   (12), 
-           (8), 
-           (17), 
-           (20), 
-           (3), 
-           (10), 
-           (19), 
-           (2), 
-           (15);
-
-INSERT INTO tbSensor (porta_sensor)
-VALUES ('A01')
-    ,('B02')
-    ,('C03')
-    ,('D04')
-    ,('E05')
-    ,('F06')
-    ,('G07')
-    ,('H08')
-    ,('I09')
-    ,('J10')
-    ,('K11')
-    ,('L12');
-
-
-INSERT INTO tbmetricas(dateMetrica, valMetrica)
-    VALUES('2023-03-16 06:40:22', 1)
-    ,('2023-03-16 06:40:28', 1)
-    ,('2023-03-16 06:45:22', 1)
-    ,('2023-03-16 06:50:22', 1);
-    
-SELECT * FROM tbUsuario;
-SELECT * FROM tbConfig;
-SELECT * FROM tbAmbiente;
-SELECT * FROM tbSensor;
-SELECT * FROM tbmetricas;
